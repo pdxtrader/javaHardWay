cf521c13b0588998a5079261dd2a6f056c4c8374
pdxtrader <benjaminblodgett@gmail.com>
2017-04-01 06:08:52 -0400

    exercise 2-24 commit

---
+++ BMICategories.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BMICategories {
+  public static void main( String[] args ) {
+    Scanner keyboard = new Scanner(System.in);
+    double bmi;
+
+    System.out.print( "Enter your BMI: " );
+    bmi = keyboard.nextDouble();
+
+    System.out.print( "BMI category: " );
+    if ( bmi < 15.0 ) {
+      System.out.println( "very severly underweight" );
+    }
+    else if ( bmi <= 16.0 ) {
+      System.out.println( "severly underweight" );
+    }
+    else if ( bmi < 18.5 ) {
+      System.out.println( "underweight" );
+    }
+    else if ( bmi < 25.0 ) {
+      System.out.println( "normal weight" );
+    }
+    else if ( bmi < 35.0 ) {
+      System.out.println( "moderately obese" );
+    }
+    else if ( bmi < 40.0 ) {
+      System.out.println( "severly obese" );
+    }
+    else {
+      System.out.println("very severly/\"morbidly\" obese" );
+    }
+  }
+}
