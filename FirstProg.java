7e96a12f362e33e1eb7855d0244db4195ca5e1bc
pdxtrader <benjaminblodgett@gmail.com>
2017-04-01 05:54:46 -0400

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main ( String[] args ) {
+		System.out.println( "I, Ben Blodgett, am determined to learn how to code." );
+		System.out.println( "Today's date is March 25th, 2017" );
+	}
+}
